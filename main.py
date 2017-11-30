f88a179fbb6ed2911c938d55a993b82fc3523097
Bogdan Vitoc <bogdanvitoc@gmail.com>
2017-11-30 16:36:14 -0500

    Clean up

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from modsim import *
 import matplotlib
 import matplotlib.animation as animation
+import pdb
+
+# Calculations
 
 def slope_func(state, t, system):
 #     x_r, y_r, vx_r, vy_r, x_p, y_p, vx_p, vy_p = state
@@ -37,33 +40,53 @@ system = System(
 
 run_odeint(system, slope_func)
 results = system.results
+
+
+##########
+# Graphing
+##########
+
+# Position
+# --------
+
 x_r = results.x_r
 y_r = results.y_r
 
+# pdb.set_trace()
+
+# Setup figure
+fig1 = plt.figure()
+fig1.set_dpi(100)
+fig1.set_size_inches(9,9)
+plt.title('Gravity Slingshot (position)')
+
+# Plot Jupiter 
+ax = plt.axes(xlim=(-5e9,5e9), ylim=(-5e9,5e9))
+ax.add_artist(plt.Circle((0,0), system.rp))
+
+# Animation
 def rocket_to_circles(t, x_r, y_r, ax):
 	x = interpolate(x_r)(t)
 	y = interpolate(y_r)(t)
 	ax.add_artist(plt.Circle((x,y), system.rp, color='r'))
 	return []
 
-fig1 = plt.figure()
-fig1.set_dpi(100)
-fig1.set_size_inches(9,9)
-
-ax = plt.axes(xlim=(-5e9,5e9), ylim=(-5e9,5e9))
-ax.add_artist(plt.Circle((0,0), system.rp))
-# plt.plot(x_r, y_r, c='black', linewidth=0.5)
-plt.title('Gravity Slingshot (position)')
-
 frames = linspace(0,duration, 50)
 ani = animation.FuncAnimation(fig1, rocket_to_circles, frames, 
 							  fargs=(x_r, y_r, ax), interval=50, blit=True)
 
 ani.save('build/slingshot.mp4', writer='ffmpeg')
-plt.show()
+
+# Velocity
+# --------
+ 
+fig_v = plt.figure()
 
 vx_r = results.vx_r
 vy_r = results.vy_r
 v_r = np.sqrt(vx_r**2 + vy_r**2)
+
 plt.plot(v_r)
 plt.title('Speed')
+
+fig_v.savefig('build/velocity.png')
